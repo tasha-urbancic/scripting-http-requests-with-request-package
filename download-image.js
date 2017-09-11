082e38aaba64a778dd2158ed391552242198bdef
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-11 20:40:44 +0000

    added console.log() statements to indicate if downloading image, and when done downloading

--- download-image.js
+++ download-image.js
@@ -1,13 +1,29 @@
-// require `request` module
+// require `request` and the Node `fs` (filesystem) module
 var request = require("request");
+var fs = require("fs");
 
-request.get('https://sytantris.github.io/http-examples/future.jpg')
+request
+  .get("https://sytantris.github.io/http-examples/future.jpg", () => {
+    console.log('Done Writing!');
+  })
   .on("error", function(err) {
     throw err;
   })
   .on("response", function(response) {
+    console.log('Downloading Image...');
     console.log("Response Status Code: ", response.statusCode);
     console.log("Response Status Message: ", response.statusMessage);
-    console.log("Response Content Type: ", response.headers['content-type']);
+    console.log("Response Content Type: ", response.headers["content-type"]);
   })
-  .pipe(fs.createWriteStream('./future.jpg'));
+  .on('end', () => {
+    console.log("Download Complete.");
+  })
+  .pipe(fs.createWriteStream("./future.jpg"));
+
+  // Downloading Image...
+  // Response Status Code:  200
+  // Response Status Message:  OK
+  // Response Content Type:  image/jpeg
+  // Download Complete.
+  // Done Writing!
+  
